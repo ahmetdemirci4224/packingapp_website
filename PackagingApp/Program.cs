01f88d38666eba70a6ec88d1ff54215b7fae7247
Ahmet <ahmet.demirci4224@gmail.com>
2026-05-23 12:18:05 +0300

    Fix Vercel 404 routing and update Backend CORS policy

--- PackagingApp/Program.cs
+++ PackagingApp/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp", policy =>
     {
-        policy.WithOrigins("http://localhost:3000", "http://localhost:5173", "http://localhost:5174", "http://localhost:5175")
+        policy.AllowAnyOrigin()
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
